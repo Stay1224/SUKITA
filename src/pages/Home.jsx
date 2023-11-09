d99aa5d5e6f01fbe2e77935e47edbb10848a3e44
stay <85134493+Stay1224@users.noreply.github.com>
2023-11-09 09:17:04 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styles from './Home.module.css';
 import K from '../Imagens/K.png';
 import arthur from '../Imagens/arthur.png';
-import pedro from '../Imagens/Pedro.png';
+import pedro from '../Imagens/pedro.png';
 
 const Home = () => {
   const [images, setImages] = useState([pedro, arthur, K]);
